95dc08302d83c7b3fb96283962ccda09857671a7
Misty-Dev <markojau@gmail.com>
2022-06-06 14:45:19 -0300

    remove boilerplate and add keys

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-Moralis.initialize("INSERT_APP_ID");
-Moralis.serverURL = "INSERT_SERVER_URL";
+Moralis.initialize('ZTRfoT2RUmXJkuQs7sN1TFZn5ASwyCVhUQHh5dhD');
+Moralis.serverURL = 'https://lsybg9ylddo6.usemoralis.com:2053/server';
 
 const login = async () => {
-    try {
-        currentUser = Moralis.User.current();
-        if (!currentUser) {
-            currentUser = await Moralis.web3.authenticate();
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
+	try {
+		currentUser = Moralis.User.current();
+		if (!currentUser) {
+			currentUser = await Moralis.web3.authenticate();
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-document.getElementById("login_button").onclick = login;
+document.getElementById('login_button').onclick = login;
