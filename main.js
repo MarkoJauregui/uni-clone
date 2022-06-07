305e6d954aeac6c2a2551850899be442075740ee
Misty-Dev <markojau@gmail.com>
2022-06-07 12:48:41 -0300

    modal close

--- main.js
+++ main.js
@@ -16,5 +16,10 @@ const openModal = () => {
 	document.getElementById('token_modal').style.display = 'block';
 };
 
+const closeModal = () => {
+	document.getElementById('token_modal').style.display = 'none';
+};
+
 document.getElementById('from_token_select').onclick = openModal;
+document.getElementById('modal_close').onclick = closeModal;
 document.getElementById('login_button').onclick = login;
