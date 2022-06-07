d2fe0a4901846c62b7e2c2e9102b41202cdc4c6c
Misty-Dev <markojau@gmail.com>
2022-06-07 13:00:00 -0300

    1inch plugin implementation

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 Moralis.initialize('ZTRfoT2RUmXJkuQs7sN1TFZn5ASwyCVhUQHh5dhD');
 Moralis.serverURL = 'https://lsybg9ylddo6.usemoralis.com:2053/server';
 
+const init = async () => {
+	await Moralis.initPlugins();
+	await Moralis.enable();
+	const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
+		chain: 'eth',
+		// The blockchain you want to use (eth/bsc/polygon)
+	});
+	console.log(tokens);
+};
+
 const login = async () => {
 	try {
 		currentUser = Moralis.User.current();
@@ -19,6 +29,8 @@ const openModal = () => {
 const closeModal = () => {
 	document.getElementById('token_modal').style.display = 'none';
 };
+
+init();
 
 document.getElementById('from_token_select').onclick = openModal;
 document.getElementById('modal_close').onclick = closeModal;
